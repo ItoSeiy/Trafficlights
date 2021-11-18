20f76cfba13e8707dd4807a43937f5204e58ff44
伊東聖矢 <tvedaamamesab@gmail.com>
2021-11-18 20:50:36 +0900

    インサートを流すテスト

---
+++ Assets/dotPeNGuinsystem/Scripts/AllSceneManeger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AllSceneManeger : MonoBehaviour
+{
+    [SerializeField] string _titleScene;
+    [SerializeField] string _slectScen;
+    [SerializeField] string _penguinSlide;
+    [SerializeField] string _penguinSpray;
+    public void TitleBack()
+    {
+        SceneManager.LoadScene(_titleScene);
+    }
+}
