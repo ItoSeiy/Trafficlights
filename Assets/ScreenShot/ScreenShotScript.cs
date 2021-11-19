c326ce3c899d213538acffa2a1f6b06a75c7ca52
1chi5o <mxculow@gmail.com>
2021-11-19 18:51:22 +0900

    スクショ制作中２

--- Assets/ScreenShot/ScreenShotScript.cs
+++ Assets/ScreenShot/ScreenShotScript.cs
@@ -8,31 +8,29 @@ using System;
 public class ScreenShotScript : MonoBehaviour
 {
 
+	[SerializeField]
+	private GameObject ShotButton;
 	//　スクリーンショットボタン
 	[SerializeField]
 	private GameObject screenShotButton;
 	//　ログパネル
 	[SerializeField]
-	private GameObject logPanel;
-	//　スクリーンショットを撮ってからの待ち時間
+	private Text logPanel;
 	[SerializeField]
 	private float waitTime = 5f;
 	//　データの保存先ファイルパス
-	private string saveFilePath = "/Projects/dotPeNGuinsystem";
+	private string saveFilePath = "/Projects/ScreenShot";
 	//　保存ファイル名
-	private string saveFileName = "/PNGZONe.PNG";
+	private string saveFileName = "/dotPNGsystem.PNG";
 
 	void Start()
 	{
-		//　UIの初期設定
-		screenShotButton.SetActive(true);
-		logPanel.SetActive(false);
 
 		//　指定したフォルダがない時はAssetsフォルダに保存
-		if (!Directory.Exists(Application.dataPath + saveFilePath))
-		{
-			saveFilePath = "";
-		}
+		//if (!Directory.Exists(Application.persistentDataPath + saveFilePath))
+		//{
+		//	saveFilePath = "";
+		//}
 	}
 
 	//　スクリーンショットボタンを押す
@@ -44,19 +42,18 @@ public class ScreenShotScript : MonoBehaviour
 	//　スクリーンショット処理
 	IEnumerator OperationUI()
 	{
-		//　スクリーンショットを撮る前にUIを全部非表示
-		logPanel.SetActive(false);
-		//　スクリーンショットを撮る
-		ScreenCapture.CaptureScreenshot(Application.persistentDataPath + saveFilePath + saveFileName,1);
+		ScreenCapture.CaptureScreenshot(Application.persistentDataPath + saveFilePath + saveFileName);
+
 		//　待ち時間を入れないとlogPanelが表示されるので一定時間待つ
 		yield return new WaitForSeconds(0.1f);
-		logPanel.transform.GetChild(0).GetComponent<Text>().text = "スクリーンショットをトりマシタ！\n" + Application.persistentDataPath + saveFilePath + saveFileName + " にホゾンしていマス。";
-		//　スクリーンショットを撮った旨を表示
-		logPanel.SetActive(true);
 		yield return new WaitForSeconds(waitTime);
-		//　UIを元に戻す
-		logPanel.SetActive(false);
+		logPanel.text = "スクリーンショットを撮りました！\n" + Application.persistentDataPath + saveFilePath + saveFileName + " に保存されました。";
+		//　スクリーンショットを撮った旨を表示
 
 	}
+	//　画面サイズをテキストに表示
+	//public void ChangeSuperSizeText(Single value)
+	//{
+	//	superSizeText.text = value.ToString();
 
 }
